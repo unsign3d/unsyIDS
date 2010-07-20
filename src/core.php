9e030a69a013dc6a63f591b6c19f2f9d7864dd17
Luca Unsigned <luca@unsigned.it>
2010-07-20 12:19:06 +0200

    Added HTTP_X_FORWARDED_FOR and populate README

--- src/core.php
+++ src/core.php
@@ -45,7 +45,7 @@ if (strcmp($dati['filters'], 'all') == 0){
 function found(){
 include 'dati.php';
 $date = date(DATE_RFC822);
-$ip = $_SERVER['REMOTE_ADDR'];
+$ip = getRealIp();
 
 if($dati['logging']==1){
  $file=fopen('log.txt', 'a+');
@@ -65,54 +65,58 @@ if($dati['alert']==1){
 	 'unsyIDS stopped an attack',
 	 //message
 	 'I just stopped an attack on your site
-	 If you have turned on the logging system you can read the list of attack stopped
-	 Date of attack: '.$date.'
-	 Query string: '.$_SERVER['QUERY_STRING'].'
-	 User Agent: '.$_SERVER['HTTP_USER_AGENT'].'
-	 Ip: '.$ip.' 
-	 Thanks for using this software,
-	 Have a nice day',
-	 'X-Mailer: unsyIDS \r\n
-	 Content-type: text; charset=iso-8859-1 \r\n,
-	 Content-Transfer-Encoding: 8bit\n\n');
+If you have turned on the logging system you can read the list of attack stopped
+Date of attack: '.$date.'
+Query string: '.$_SERVER['QUERY_STRING'].'
+User Agent: '.$_SERVER['HTTP_USER_AGENT'].'
+Ip: '.$ip.' 
+Thanks for using this software,
+Have a nice day',
+'X-Mailer: unsyIDS \r\n
+Content-type: text; charset=iso-8859-1 \r\n,
+Content-Transfer-Encoding: 8bit\n\n');
 }
  
  if ($dati['alert'] == 1){ 
-			echo '
-								 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
-								 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
-								 <html xmlns="http://www.w3.org/1999/xhtml" lang="it"> 
-								 <head> 
-								 <title>Buhahahahahaha</title> 
-								 <!-- meta tag --> 
-								 <meta http-equiv="Content-type" content="text/html; charset=utf-8" /> 
-								 <meta http-equiv="Content-Language" content="en" /> 
-								 <meta name="Keywords" content="unsigned space" /> 
-								 <meta name="Owner" content="Luca \'Unsigned\' <luca@unsigned.it>" /> 
-								 <meta name="Author" content="Luca \'Unsigned\' <luca@unsigned.it)" /> 
-								 <meta name="distribution" content="Global" />  
-								 <meta name="generator" content="Vim" /> 
-								 <meta name="cms" content="unsyIDS" /> 
-								 <meta name="Copyright" content="unsigned.it" /> 
-								 <style> 
-								 	body{
-										background-color: #000000;
-										color:#00ff00;
-										text-align:center;
-									}
-								</style> 
-				 				</head> 
-				  				<body> 
-				  				<h1>Lolz you fail</h1> 
-								<p>Attack found from '.$ip.'</p>
-								<p>Powered by unsyIDS</p>
-								</body> 
-				  				</html> ';
+	echo '
+		 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
+		 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
+		 <html xmlns="http://www.w3.org/1999/xhtml" lang="it"> 
+		 <head> 
+		 <title>Buhahahahahaha</title> 
+		 <!-- meta tag --> 
+		 <meta http-equiv="Content-type" content="text/html; charset=utf-8" /> 
+		 <meta http-equiv="Content-Language" content="en" /> 
+		 <meta name="Keywords" content="unsigned space" /> 
+		 <meta name="Owner" content="Luca \'Unsigned\' <luca@unsigned.it>" /> 
+		 <meta name="Author" content="Luca \'Unsigned\' <luca@unsigned.it)" /> 
+		 <meta name="distribution" content="Global" />  
+		 <meta name="generator" content="Vim" /> 
+		 <meta name="cms" content="unsyIDS" /> 
+		 <meta name="Copyright" content="unsigned.it" /> 
+		 <style> 
+			body{
+				background-color: #000000;
+				color:#00ff00;
+				text-align:center;
 			}
+		</style> 
+		</head> 
+		<body> 
+		<h1>Lolz you fail</h1> 
+		<p>Attack found from '.$ip.'</p>
+		<p>Powered by unsyIDS</p>
+		</body> 
+		</html> ';
+		}
 		die();
 	}
 
 	function sentinel(){
 		filter();
+	}
+	
+	function getRealIp(){
+		return isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : $_SERVER['REMOTE_ADDR'];
 	}
 ?>
